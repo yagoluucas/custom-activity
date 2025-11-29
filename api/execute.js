aa6e2f13646e04d499c627cec5148ef460690833
yagoluucas <yago543@gmail.com>
2025-11-29 14:50:08 -0300

    ajuste

--- api/execute.js
+++ api/execute.js
@@ -1,10 +1,8 @@
 export default function execute(req, res) {
   console.log("=== EXECUTE ===");
   try {
-    if(req.body && req.body.inArguments && req.body.inArguments.length > 0) {
-      const inArgs = req.body.inArguments[0];
-      console.log("inArguments:", inArgs);
-    }
+    const inArgs = req.body.inArguments[0];
+    console.log("inArguments:", inArgs);
     res.status(200).json({
       outArguments: [{ resultado: "success" }],
     });
