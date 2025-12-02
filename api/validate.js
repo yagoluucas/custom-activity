588730495f1b3bfe425a93baaaf40d44875e26db
yagoluucas <yago543@gmail.com>
2025-12-01 21:38:58 -0300

    Ajustado config

--- api/validate.js
+++ api/validate.js
@@ -1,11 +1,11 @@
-export function validate(req, res) {
-    console.log("=== VALIDATE ===");
+export default function stop(req, res) {
+    console.log("===  VALIDATE ===");
     try {
         const inArgs = req.body.inArguments[0];
         console.log("inArguments:", inArgs);
-        res.status(200).json({ status: "Validation successful" });
+        res.status(200).json({ status: "Activity stopped successfully" });
     } catch (error) {
-        console.error("Error in /validate:", error);
+        console.error("Error in /stop:", error);
         res.status(400).json({ error: error.message });
     }
 }
