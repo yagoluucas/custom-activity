6e391157631fc4bc6665d71b93a92dc92a3c27e4
yagoluucas <yago543@gmail.com>
2025-12-03 19:24:58 -0300

    Ajuste de config

--- customActivity.js
+++ customActivity.js
@@ -7,42 +7,44 @@ define(["postmonger"], function (Postmonger) {
   connection.on("initActivity", function (incomingPayload) {
     payload = incomingPayload;
 
-    payload.metaData.isConfigured = true;
-
     const inArgs = payload?.arguments?.execute?.inArguments;
 
     console.log("Saved Arguments: ", inArgs);
   });
 
-  // connection.on("clickedNext", function () {
-  //   const deName = document.getElementById("deName").value;
-  //   const labelName = document.getElementById("labelName").value;
+  connection.on("clickedNext", function () {
+    const deName = document.getElementById("deName").value;
+    const labelName = document.getElementById("labelName").value;
 
-  //   if (!deName || !labelName) {
-  //     alert("Por favor, preencha todos os campos obrigatórios.");
-  //   }
+    if (!deName || !labelName) {
+      alert("Por favor, preencha todos os campos obrigatórios.");
+    }
 
-  //   // pega os argumentos já existentes no config.json (como email/contactKey)
-  //   let existingArgs = payload?.arguments?.execute?.inArguments || {};
+    // let existingArgs = payload?.arguments?.execute?.inArguments || {};
 
-  //   // transforma em um objeto só
-  //   let mergedArgs = Object.assign({}, existingArgs);
+    payload?.arguments?.execute?.inArguments = [
+      {email: "yagodoteste123@pmweb.com.br"},
+      {contactKey: "{{Contact.Key}}"}
+    ]
 
-  //   // adiciona os novos valores
-  //   mergedArgs.deName = deName;
-  //   mergedArgs.labelName = labelName;
+    // transforma em um objeto só
+    // let mergedArgs = Object.assign({}, existingArgs);
 
-  //   // reatribui como array de objetos (formato que a SFMC exige)
-  //   payload.arguments.execute.inArguments = Object.keys(mergedArgs).map(key => ({
-  //     [key]: mergedArgs[key],
-  //   }));
+    // // adiciona os novos valores
+    // mergedArgs.deName = deName;
+    // mergedArgs.labelName = labelName;
 
-  //   console.log("Payload final: ", payload);
+    // // reatribui como array de objetos (formato que a SFMC exige)
+    // payload.arguments.execute.inArguments = Object.keys(mergedArgs).map(key => ({
+    //   [key]: mergedArgs[key],
+    // }));
 
-  //   payload.metaData.isConfigured = true;
+    console.log("Payload final: ", payload);
 
-  //   connection.trigger("updateActivity", payload);
-  // });
+    payload.metaData.isConfigured = true;
+
+    connection.trigger("updateActivity", payload);
+  });
 
   connection.trigger("ready");
 });
